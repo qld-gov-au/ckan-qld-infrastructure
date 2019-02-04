53c457e243fb77c9a86288a40ea8c223ca7807a3
antuarc <carl.antuar@dsiti.qld.gov.au>
2019-02-04 16:07:36 +1000

    [QOL-4495] add network security groups

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@
 set -e
 
 ansible-playbook -i inventory/hosts "vpc.yml" --extra-vars "Environment=$bamboo_deploy_environment" -vvv
-
+ansible-playbook -i inventory/hosts "security_groups.yml" --extra-vars "Environment=$bamboo_deploy_environment" -vvv
 ansible-playbook -i inventory/hosts "database.yml" --extra-vars "Environment=$bamboo_deploy_environment" -vvv
