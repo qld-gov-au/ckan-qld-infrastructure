3f28a728179ec202f46026ea8936d61cd38e00c6
antuarc <carl.antuar@dsiti.qld.gov.au>
2019-02-04 13:45:25 +1000

    [QOL-4495] initial CloudFormation templates for setting up CKAN
    - Based on DataShades templates at https://github.com/DataShades/ckan-aws-templates/

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#deploy CKAN base infrastructure
+
+#ensure we die if any function fails
+set -e
+
+ansible-playbook -i inventory/hosts "vpc.yml" --extra-vars "Environment=$bamboo_deploy_environment" -vvv
+
+ansible-playbook -i inventory/hosts "database.yml" --extra-vars "Environment=$bamboo_deploy_environment" -vvv
