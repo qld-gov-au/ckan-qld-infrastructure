69d71f17d808b02efe8ec0c14ddd11f67be79f89
antuarc <carl.antuar@dsiti.qld.gov.au>
2019-02-05 13:17:18 +1000

    [QOL-4495] run the EFS template before building the main stack

--- build.sh
+++ build.sh
@@ -7,4 +7,5 @@ set -e
 ansible-playbook -i inventory/hosts "vpc.yml" --extra-vars "Environment=$bamboo_deploy_environment" -vvv
 ansible-playbook -i inventory/hosts "security_groups.yml" --extra-vars "Environment=$bamboo_deploy_environment" -vvv
 ansible-playbook -i inventory/hosts "database.yml" --extra-vars "Environment=$bamboo_deploy_environment" -vvv
+ansible-playbook -i inventory/hosts "efs.yml" --extra-vars "Environment=$bamboo_deploy_environment" -vvv
 ansible-playbook -i inventory/hosts "CKAN-Stack.yml" --extra-vars "Environment=$bamboo_deploy_environment" -vvv
