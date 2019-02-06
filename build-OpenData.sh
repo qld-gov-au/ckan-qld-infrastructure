20fb6b42d85e6cd6ac8333b2e537816ea125f875
antuarc <carl.antuar@dsiti.qld.gov.au>
2019-02-06 11:52:02 +1000

    [QOL-4495] collapse database config back into a shared RDS instance
    - Specific databases can be configured at a later step
    - Also configure parameters to allow easy tear-down

--- build-OpenData.sh
+++ build-OpenData.sh
@@ -6,6 +6,6 @@ set -e
 
 ansible-playbook -i inventory/hosts "vpc.yml" --extra-vars "Environment=$bamboo_deploy_environment" -vvv
 ansible-playbook -i inventory/hosts "security_groups.yml" --extra-vars "Environment=$bamboo_deploy_environment" -vvv
-ansible-playbook -i inventory/hosts "OpenData-database.yml" --extra-vars "Environment=$bamboo_deploy_environment" -vvv
+ansible-playbook -i inventory/hosts "database.yml" --extra-vars "Environment=$bamboo_deploy_environment" -vvv
 ansible-playbook -i inventory/hosts "OpenData-efs.yml" --extra-vars "Environment=$bamboo_deploy_environment" -vvv
 ansible-playbook -i inventory/hosts "OpenData-CKAN-Stack.yml" --extra-vars "Environment=$bamboo_deploy_environment" -vvv
--- build-Publications.sh
+++ build-Publications.sh
@@ -6,6 +6,6 @@ set -e
 
 ansible-playbook -i inventory/hosts "vpc.yml" --extra-vars "Environment=$bamboo_deploy_environment" -vvv
 ansible-playbook -i inventory/hosts "security_groups.yml" --extra-vars "Environment=$bamboo_deploy_environment" -vvv
-ansible-playbook -i inventory/hosts "Publications-database.yml" --extra-vars "Environment=$bamboo_deploy_environment" -vvv
+ansible-playbook -i inventory/hosts "database.yml" --extra-vars "Environment=$bamboo_deploy_environment" -vvv
 ansible-playbook -i inventory/hosts "Publications-efs.yml" --extra-vars "Environment=$bamboo_deploy_environment" -vvv
 ansible-playbook -i inventory/hosts "Publications-CKAN-Stack.yml" --extra-vars "Environment=$bamboo_deploy_environment" -vvv
