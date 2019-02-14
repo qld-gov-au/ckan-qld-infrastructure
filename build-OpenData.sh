038167a5b4bbcc73256f1d739ad089e599cb24f9
antuarc <carl.antuar@dsiti.qld.gov.au>
2019-02-14 15:00:47 +1000

    [QOL-4495] point OpsWorks stack at Redis cluster

--- build-OpenData.sh
+++ build-OpenData.sh
@@ -10,6 +10,6 @@ ansible-playbook -i inventory/hosts "vpc.yml" --extra-vars "$ANSIBLE_EXTRA_VARS"
 ansible-playbook -i inventory/hosts "security_groups.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
 ansible-playbook -i inventory/hosts "database.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
 ansible-playbook -i inventory/hosts "efs.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
-# ansible-playbook -i inventory/hosts "OpenData-database.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
+ansible-playbook -i inventory/hosts "cache.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
 ansible-playbook -i inventory/hosts "OpenData-CKAN-Stack.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
 ansible-playbook -i inventory/hosts "OpenData-CKAN-instances.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
