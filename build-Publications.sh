3e7be6bbafe9208a5587d15b539234f6f022a753
antuarc <carl.antuar@dsiti.qld.gov.au>
2019-02-15 14:57:52 +1000

    [QOL-4495] update Publications CloudFormation to match Open Data

--- build-Publications.sh
+++ build-Publications.sh
@@ -4,8 +4,12 @@
 #ensure we die if any function fails
 set -e
 
-ansible-playbook -i inventory/hosts "vpc.yml" --extra-vars "Environment=$bamboo_deploy_environment" -vvv
-ansible-playbook -i inventory/hosts "security_groups.yml" --extra-vars "Environment=$bamboo_deploy_environment" -vvv
-ansible-playbook -i inventory/hosts "database.yml" --extra-vars "Environment=$bamboo_deploy_environment" -vvv
-ansible-playbook -i inventory/hosts "efs.yml" --extra-vars "Environment=$bamboo_deploy_environment" -vvv
-ansible-playbook -i inventory/hosts "Publications-CKAN-Stack.yml" --extra-vars "Environment=$bamboo_deploy_environment" -vvv
+ANSIBLE_EXTRA_VARS="$ANSIBLE_EXTRA_VARS Environment=$bamboo_deploy_environment"
+
+ansible-playbook -i inventory/hosts "vpc.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
+ansible-playbook -i inventory/hosts "security_groups.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
+ansible-playbook -i inventory/hosts "database.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
+ansible-playbook -i inventory/hosts "efs.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
+ansible-playbook -i inventory/hosts "cache.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
+ansible-playbook -i inventory/hosts "Publications-CKAN-Stack.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
+ansible-playbook -i inventory/hosts "Publications-CKAN-instances.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
