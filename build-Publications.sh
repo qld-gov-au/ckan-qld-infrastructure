acbabf1fcdc53fbbb98db966accd61bf24d6f4f7
antuarc <carl.antuar@dsiti.qld.gov.au>
2019-02-18 13:11:07 +1000

    [QOL-4495] fix database config parameters
    - make template_parameters visible to role
    - provide Publications parameters in shell script
    - invoke database config from shell scripts

--- build-Publications.sh
+++ build-Publications.sh
@@ -4,11 +4,12 @@
 #ensure we die if any function fails
 set -e
 
-ANSIBLE_EXTRA_VARS="$ANSIBLE_EXTRA_VARS Environment=$bamboo_deploy_environment"
+ANSIBLE_EXTRA_VARS="$ANSIBLE_EXTRA_VARS titlecase_name=Publications lowercase_name=publications Environment=$bamboo_deploy_environment"
 
 ansible-playbook -i inventory/hosts "vpc.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
 ansible-playbook -i inventory/hosts "security_groups.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
 ansible-playbook -i inventory/hosts "database.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
+ansible-playbook -i inventory/hosts "database-config.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
 ansible-playbook -i inventory/hosts "efs.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
 ansible-playbook -i inventory/hosts "cache.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
 ansible-playbook -i inventory/hosts "Publications-CKAN-Stack.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
