3e4587e4e5d2746c1a3aff9a278fa135ce638c8c
antuarc <carl.antuar@dsiti.qld.gov.au>
2019-02-18 12:54:47 +1000

    [QOL-4495] define app-specific var in shell script instead of directly in playbook

--- build-OpenData.sh
+++ build-OpenData.sh
@@ -4,7 +4,7 @@
 #ensure we die if any function fails
 set -e
 
-ANSIBLE_EXTRA_VARS="$ANSIBLE_EXTRA_VARS Environment=$bamboo_deploy_environment"
+ANSIBLE_EXTRA_VARS="$ANSIBLE_EXTRA_VARS titlecase_name=OpenData lowercase_name=opendata Environment=$bamboo_deploy_environment"
 
 ansible-playbook -i inventory/hosts "vpc.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
 ansible-playbook -i inventory/hosts "security_groups.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
