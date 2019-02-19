b88aecd808cd4a95acbfd1113e0daa54938e0ea4
antuarc <carl.antuar@dsiti.qld.gov.au>
2019-02-19 13:45:57 +1000

    [QOL-4495] continue even after VPC error since they can happen without being fatal

--- build-Publications.sh
+++ build-Publications.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 #deploy CKAN base infrastructure
 
-#ensure we die if any function fails
-set -e
 
 ANSIBLE_EXTRA_VARS="$ANSIBLE_EXTRA_VARS region=ap-southeast-2 Environment=$bamboo_deploy_environment titlecase_name=Publications lowercase_name=publications"
 
 ansible-playbook -i inventory/hosts "vpc.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
+
+#ensure we die if any function fails after this
+set -e
+
 ansible-playbook -i inventory/hosts "security_groups.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
 ansible-playbook -i inventory/hosts "database.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
 ansible-playbook -i inventory/hosts "database-config.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
