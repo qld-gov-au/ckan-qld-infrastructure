d2365ae3331392fdb1f8eb1fde8ed6b2ab4eb04f
antuarc <carl.antuar@dsiti.qld.gov.au>
2019-02-27 12:38:59 +1000

    [QOL-4533] improve build scripts
    - allow running a single playbook
    - fix error setting to fail fast on any playbook including VPC
    - enable datastore support for Open Data

--- build-Publications.sh
+++ build-Publications.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 #deploy CKAN base infrastructure
 
-
 ANSIBLE_EXTRA_VARS="$ANSIBLE_EXTRA_VARS region=ap-southeast-2 Environment=$bamboo_deploy_environment titlecase_name=Publications lowercase_name=publications"
 
-ansible-playbook -i inventory/hosts "vpc.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
+if [ $# -ge 1 ]; then
+  # run custom playbook
+  PLAYBOOK=$1.yml
+  ansible-playbook -i inventory/hosts "$PLAYBOOK" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
+else
+  #ensure we die if any function fails
+  set -e
 
-#ensure we die if any function fails after this
-set -e
+  ansible-playbook -i inventory/hosts "vpc.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
+  ansible-playbook -i inventory/hosts "security_groups.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
+  ansible-playbook -i inventory/hosts "database.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
+  ansible-playbook -i inventory/hosts "database-config.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
+  ansible-playbook -i inventory/hosts "efs.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
+  ansible-playbook -i inventory/hosts "cache.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
+  ansible-playbook -i inventory/hosts "CKAN-Stack.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
+  ansible-playbook -i inventory/hosts "CKAN-extensions.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
+  ansible-playbook -i inventory/hosts "CKAN-instances.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
+fi
 
-ansible-playbook -i inventory/hosts "security_groups.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
-ansible-playbook -i inventory/hosts "database.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
-ansible-playbook -i inventory/hosts "database-config.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
-ansible-playbook -i inventory/hosts "efs.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
-ansible-playbook -i inventory/hosts "cache.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
-ansible-playbook -i inventory/hosts "CKAN-Stack.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
-ansible-playbook -i inventory/hosts "CKAN-extensions.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
-ansible-playbook -i inventory/hosts "CKAN-instances.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
