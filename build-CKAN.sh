2b97beec7ca0de73cc358a9867f19747e37735f9
antuarc <carl.antuar@dsiti.qld.gov.au>
2019-03-07 11:11:08 +1000

    [QOL-4415] clean up templates and vars dquote> dquote> - move VPC peering into its own playbook dquote> - extract more string literals into vars dquote> - move command-line vars into files dquote> - extract common shell functionality for Open Data and Publications dquote> - collapse environment-specific stack definitions when vars are enough dquote> - drop unused and deprecated DataShades templates - drop unnecessary explicit dependency declarations when implicit is enough

---
+++ build-CKAN.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#deploy CKAN base infrastructure
+
+VARS_FILE="$1"
+ENVIRONMENT="$2"
+ANSIBLE_EXTRA_VARS="$ANSIBLE_EXTRA_VARS Environment=$ENVIRONMENT"
+
+function run-playbook {
+  ansible-playbook -i inventory/hosts "$1.yml" --extra-vars "@$VARS_FILE" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
+}
+
+if [ $# -ge 3 ]; then
+  # run custom playbook
+  run-playbook "$3"
+else
+  #ensure we die if any function fails
+  set -e
+
+  run-playbook "vpc"
+  run-playbook "vpc-config"
+  run-playbook "security_groups"
+  run-playbook "database"
+  run-playbook "database-config"
+  run-playbook "efs"
+  run-playbook "cache"
+  run-playbook "waf"
+  run-playbook "CKAN-Stack"
+  run-playbook "CKAN-extensions"
+  run-playbook "CKAN-instances"
+  run-playbook "cloudfront"
+fi
+
--- build-Publications.sh
+++ build-Publications.sh
@@ -1,29 +1,4 @@
 #!/bin/bash
-#deploy CKAN base infrastructure
+#deploy Publications base infrastructure
 
-ANSIBLE_EXTRA_VARS="$ANSIBLE_EXTRA_VARS region=ap-southeast-2 Environment=$bamboo_deploy_environment titlecase_name=Publications lowercase_name=publications"
-
-function run-playbook {
-  ansible-playbook -i inventory/hosts "$1.yml" --extra-vars "$ANSIBLE_EXTRA_VARS" -vvv
-}
-
-if [ $# -ge 1 ]; then
-  # run custom playbook
-  run-playbook "$1"
-else
-  #ensure we die if any function fails
-  set -e
-
-  run-playbook "vpc"
-  run-playbook "security_groups"
-  run-playbook "database"
-  run-playbook "database-config"
-  run-playbook "efs"
-  run-playbook "cache"
-  run-playbook "waf"
-  run-playbook "CKAN-Stack"
-  run-playbook "CKAN-extensions"
-  run-playbook "CKAN-instances"
-  run-playbook "cloudfront"
-fi
-
+./build-CKAN.sh vars/Publications.yml $bamboo_deploy_environment $1
