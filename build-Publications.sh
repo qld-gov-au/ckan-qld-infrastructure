cb5012645b8ebb4ff5870602c5f01a9aa6962b9a
antuarc <carl.antuar@smartservice.qld.gov.au>
2019-10-11 15:20:20 +1000

    [QOL-5814] clean up deployment process
    - fix parallel deployments so 'update_custom_cookbooks' and 'configure' can run on all instances at once
    - refactor to allow easy full deployments (update cookbooks on all, setup each web instance, configure all)

--- build-Publications.sh
+++ build-Publications.sh
@@ -5,6 +5,13 @@ INSTANCE_NAME=Publications
 INSTANCE_SHORTNAME=`echo $INSTANCE_NAME |tr '[A-Z]' '[a-z]'`
 STACK_NAME="${INSTANCE_NAME}_$bamboo_deploy_environment"
 
+run-deployment () {
+  PARALLEL=1 ./opsworks-deploy.sh update_custom_cookbooks $STACK_NAME
+  ./opsworks-deploy.sh setup $STACK_NAME ${INSTANCE_SHORTNAME}-web
+  ./opsworks-deploy.sh execute_recipes $STACK_NAME ${INSTANCE_SHORTNAME}-solr datashades::solr-deploy
+  PARALLEL=1 ./opsworks-deploy.sh configure $STACK_NAME
+}
+
 run-all-playbooks () {
   #ensure we die if any function fails
   set -e
@@ -17,10 +24,7 @@ run-all-playbooks () {
   run-playbook "CloudFormation" "vars/CKAN-instances.var.yml"
   run-playbook "CloudFormation" "vars/cloudfront-lambda-at-edge.var.yml"
   run-playbook "cloudfront"
-  PARALLEL=1 ./opsworks-deploy.sh update_custom_cookbooks $STACK_NAME
-  ./opsworks-deploy.sh setup $STACK_NAME ${INSTANCE_SHORTNAME}-web
-  ./opsworks-deploy.sh execute_recipes $STACK_NAME ${INSTANCE_SHORTNAME}-solr datashades::solr-deploy
-  PARALLEL=1 ./opsworks-deploy.sh configure $STACK_NAME
+  run-deployment
 }
 
 . ./build-CKAN.sh vars/shared-$INSTANCE_NAME.var.yml $bamboo_deploy_environment $@
