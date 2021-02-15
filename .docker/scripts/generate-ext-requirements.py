99a3a53079104c26942b2d01dd1f376fca8f9622
ThrawnCA <shell_layer-github@yahoo.com.au>
2021-02-15 15:51:34 +1000

    [QOL-5815] install dependencies in 'editable' mode to more closely match old system

--- .docker/scripts/generate-ext-requirements.py
+++ .docker/scripts/generate-ext-requirements.py
@@ -8,7 +8,7 @@ target_environment = os.environ.get('DEPLOY_ENV', 'DEV')
 requirement_list = ''
 extensions = yaml.safe_load(open(extensions_file))['extensions'][target_environment]
 for key, value in six.iteritems(extensions):
-    requirement_list += 'git+{}@{}#egg={}\n'.format(value['url'], value['version'], value['shortname'])
+    requirement_list += '-e git+{}@{}#egg={}\n'.format(value['url'], value['version'], value['shortname'])
 
 requirement_file = open('/app/requirements-ext.txt', 'w')
 requirement_file.write(requirement_list)
