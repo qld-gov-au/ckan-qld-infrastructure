7815826dc7c028baefe308d88caf3e5d8086d676
ThrawnCA <shell_layer-github@yahoo.com.au>
2021-02-15 15:18:37 +1000

    [QOL-5815] rename target environment parameter to avoid possible OS clashes

--- .docker/scripts/generate-ext-requirements.py
+++ .docker/scripts/generate-ext-requirements.py
@@ -4,7 +4,7 @@ import six
 import yaml
 
 extensions_file = os.environ.get('EXTENSIONS_FILE', 'extensions.yml')
-target_environment = os.environ.get('ENV', 'DEV')
+target_environment = os.environ.get('DEPLOY_ENV', 'DEV')
 requirement_list = ''
 extensions = yaml.safe_load(open(extensions_file))['extensions'][target_environment]
 for key, value in six.iteritems(extensions):
