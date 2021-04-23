258695cf5a7248a557aa4d86a2d227f1b85afa68
antuarc <carl.antuar@smartservice.qld.gov.au>
2021-04-23 10:59:11 +1000

    [QOL-7857] clean up test config for ckanext-qgov changes
    - provide full/displayed name for user accounts
    - use cli wrapper to be more compatible with CKAN 2.9

--- .docker/scripts/create-test-data.sh
+++ .docker/scripts/create-test-data.sh
@@ -6,23 +6,30 @@
 set -e
 
 CKAN_ACTION_URL=http://ckan:3000/api/action
-CKAN_INI_FILE=/app/ckan/default/production.ini
+CKAN_INI=/app/ckan/default/production.ini
 
-. /app/ckan/default/bin/activate \
-    && cd /app/ckan/default/src/ckan
+. /app/ckan/default/bin/activate
+
+ckan_cli () {
+    if (which ckan > /dev/null); then
+        ckan -c ${CKAN_INI} "$@"
+    else
+        paster --plugin=ckan "$@" -c ${CKAN_INI}
+    fi
+}
 
 # We know the "admin" sysadmin account exists, so we'll use her API KEY to create further data
-API_KEY=$(paster --plugin=ckan user admin -c ${CKAN_INI_FILE} | tr -d '\n' | sed -r 's/^(.*)apikey=(\S*)(.*)/\2/')
+API_KEY=$(ckan_cli user admin | tr -d '\n' | sed -r 's/^(.*)apikey=(\S*)(.*)/\2/')
 
-# Creating test data hierarchy which creates organisations assigend to datasets
-paster create-test-data hierarchy -c ${CKAN_INI_FILE}
+# Creating test data hierarchy which creates organisations assigned to datasets
+ckan_cli create-test-data hierarchy
 
 # Creating basic test data which has datasets with resources
-paster create-test-data -c ${CKAN_INI_FILE}
+ckan_cli create-test-data
 
 echo "Updating annakarenina to use department-of-health Organisation:"
 package_owner_org_update=$( \
-    curl -L -s -O --header "Authorization: ${API_KEY}" \
+    curl -L -s --header "Authorization: ${API_KEY}" \
     --data "id=annakarenina&organization_id=department-of-health" \
     ${CKAN_ACTION_URL}/package_owner_org_update
 )
