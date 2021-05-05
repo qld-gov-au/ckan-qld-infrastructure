e4cb2663d83c9eca22d71b88ba17f6c426628e86
ThrawnCA <shell_layer-github@yahoo.com.au>
2021-05-06 09:29:29 +1000

    [QOL-5815] fix extension installation
    - fix path to generated requirement file
    - update list of plugins for split ckanext-data-qld

--- .docker/scripts/init-ext.sh
+++ .docker/scripts/init-ext.sh
@@ -10,7 +10,7 @@ pip install -r "/app/requirements.txt"
 pip install -r "/app/requirements-dev.txt"
 #python setup.py develop
 EXTENSIONS_FILE=/app/scripts/extensions.yml python $(dirname $0)/generate-ext-requirements.py
-pip install --force-reinstall -r "/app/requirements-ext.txt"
+pip install --force-reinstall -r "/tmp/requirements-ext.txt"
 
 # Validate that the extension was installed correctly.
 #if ! pip list | grep ckan-qld-infrastructure > /dev/null; then echo "Unable to find the extension in the list"; exit 1; fi
