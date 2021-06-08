bb688aafffa0a38e29af0436562c8680faefb389
ThrawnCA <shell_layer-github@yahoo.com.au>
2021-06-09 08:47:26 +1000

    [QOL-8048] use virtualenv Python to generate requirements file

--- .docker/scripts/init-ext.sh
+++ .docker/scripts/init-ext.sh
@@ -8,6 +8,6 @@ PIP="${APP_DIR}/bin/pip"
 cd $WORKDIR
 $PIP install -r "requirements.txt"
 $PIP install -r "requirements-dev.txt"
-EXTENSIONS_FILE=$WORKDIR/scripts/extensions.yml python $(dirname $0)/generate-ext-requirements.py
+EXTENSIONS_FILE=$WORKDIR/scripts/extensions.yml $APP_DIR/bin/python $(dirname $0)/generate-ext-requirements.py
 $PIP install --force-reinstall -r "/app/requirements-ext.txt"
 
