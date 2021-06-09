d09a7ed2ea022647122bc0a30ccafcd773372481
ThrawnCA <shell_layer-github@yahoo.com.au>
2021-06-09 11:51:59 +1000

    [QOL-8048] add build runs for Publications

--- .docker/scripts/init-ext.sh
+++ .docker/scripts/init-ext.sh
@@ -9,5 +9,5 @@ cd $WORKDIR
 $PIP install -r "requirements.txt"
 $PIP install -r "requirements-dev.txt"
 EXTENSIONS_FILE=$WORKDIR/scripts/extensions.yml $APP_DIR/bin/python $(dirname $0)/generate-ext-requirements.py
-$PIP install --force-reinstall -r "/app/requirements-ext.txt"
+$PIP install --force-reinstall -r "/tmp/requirements-ext.txt"
 
