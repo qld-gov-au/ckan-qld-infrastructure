567defc93758d235bdddf0a14a9b4ab947dd30bf
ThrawnCA <shell_layer-github@yahoo.com.au>
2021-08-13 16:30:21 +1000

    [QOL-8117] clean up CI scripts for better consistency

--- .docker/scripts/init-ext.sh
+++ .docker/scripts/init-ext.sh
@@ -4,10 +4,14 @@
 #
 set -e
 
-PIP="${APP_DIR}/bin/pip"
-cd $WORKDIR
-$PIP install -r "requirements.txt"
-$PIP install -r "requirements-dev.txt"
-EXTENSIONS_FILE=$WORKDIR/scripts/extensions.yml $APP_DIR/bin/python $(dirname $0)/generate-ext-requirements.py
-$PIP install --force-reinstall -r "/tmp/requirements-ext.txt"
+if [ "$VENV_DIR" != "" ]; then
+  . ${VENV_DIR}/bin/activate
+fi
+pip install -r "requirements.txt"
+pip install -r "requirements-dev.txt"
+EXTENSIONS_FILE=$APP_DIR/scripts/extensions.yml $APP_DIR/bin/python $(dirname $0)/generate-ext-requirements.py
+pip install --force-reinstall -r "/tmp/requirements-ext.txt"
 
+if [ "$VENV_DIR" != "" ]; then
+  deactivate
+fi
--- test/features/environment.py
+++ test/features/environment.py
@@ -14,11 +14,37 @@ REMOTE_CHROME_URL = 'http://chrome:4444/wd/hub'
 
 # @see .docker/scripts/init.sh for credentials.
 PERSONAS = {
-    'Admin': dict(
-        name=u'admin',
-        email=u'admin@localhost',
-        password=u'Password123!'
-    )
+    'SysAdmin': {
+        'name': u'admin',
+        'email': u'admin@localhost',
+        'password': u'Password123!'
+    },
+    'Unauthenticated': {
+        'name': u'',
+        'email': u'',
+        'password': u''
+    },
+    # This user will not be assigned to any organisations
+    'CKANUser': {
+        'name': u'ckan_user',
+        'email': u'ckan_user@localhost',
+        'password': u'Password123!'
+    },
+    'TestOrgAdmin': {
+        'name': u'test_org_admin',
+        'email': u'test_org_admin@localhost',
+        'password': u'Password123!'
+    },
+    'TestOrgEditor': {
+        'name': u'test_org_editor',
+        'email': u'test_org_editor@localhost',
+        'password': u'Password123!'
+    },
+    'TestOrgMember': {
+        'name': u'test_org_member',
+        'email': u'test_org_member@localhost',
+        'password': u'Password123!'
+    }
 }
 
 
@@ -27,6 +53,8 @@ def before_all(context):
     context.screenshots_dir = os.path.join(ROOT_PATH, 'test/screenshots')
     # The path where file attachments can be found.
     context.attachment_dir = os.path.join(ROOT_PATH, 'test/fixtures')
+    # The path where emails can be found.
+    context.mail_path = os.path.join(ROOT_PATH, 'test/emails')
 
     # Set base url for all relative links.
     context.base_url = BASE_URL
