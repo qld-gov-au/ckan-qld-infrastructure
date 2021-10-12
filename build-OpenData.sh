815ae260c3295d9d7ea20f6536eae3140909a7a6
ThrawnCA <shell_layer-github@yahoo.com.au>
2021-10-12 10:08:26 +1000

    [QOL-8179] clean up build scripts

--- build-OpenData.sh
+++ build-OpenData.sh
@@ -9,22 +9,4 @@ INSTANCE_NAME=OpenData
 INSTANCE_SHORTNAME=`echo $INSTANCE_NAME |tr '[A-Z]' '[a-z]'`
 STACK_NAME="${INSTANCE_NAME}_$bamboo_deploy_environment"
 
-run-all-playbooks () {
-  #ensure we die if any function fails
-  set -e
-
-  run-shared-resource-playbooks
-  run-playbook "CloudFormation" "vars/acm.var.yml"
-  if [ "$SKIP_CKAN_DB" != "true" ]; then
-    run-playbook "database-config"
-  fi
-  run-playbook "CloudFormation" "vars/s3_buckets.var.yml"
-  run-playbook "CKAN-Stack"
-  run-playbook "CKAN-extensions"
-  run-playbook "CloudFormation" "vars/${INSTANCE_NAME}-instances.var.yml"
-  run-playbook "CloudFormation" "vars/cloudfront-lambda-at-edge.var.yml"
-  run-playbook "cloudfront"
-  run-deployment
-}
-
 . ./build-CKAN.sh vars/shared-$INSTANCE_NAME.var.yml $bamboo_deploy_environment $@
