2768c1ef57afc2c0a295135f1a4047bba4e48107
ThrawnCA <shell_layer-github@yahoo.com.au>
2021-10-21 15:14:48 +1000

    [QOL-8117] clean up
    - update test CKAN
    - add job workers and test data for testing other extensions

--- .docker/scripts/create-test-data.sh
+++ .docker/scripts/create-test-data.sh
@@ -10,8 +10,56 @@ if [ "$VENV_DIR" != "" ]; then
   . ${VENV_DIR}/bin/activate
 fi
 
+add_user_if_needed () {
+    echo "Adding user '$2' ($1) with email address [$3]"
+    ckan_cli user "$1" | grep "$1" || ckan_cli user add "$1"\
+        fullname="$2"\
+        email="$3"\
+        password="${4:-Password123!}"
+}
+
 # We know the "admin" sysadmin account exists, so we'll use her API KEY to create further data
 API_KEY=$(ckan_cli user admin | tr -d '\n' | sed -r 's/^(.*)apikey=(\S*)(.*)/\2/')
+
+##
+# BEGIN: Create a test organisation with test users for admin, editor and member
+#
+TEST_ORG_NAME=test-organisation
+TEST_ORG_TITLE="Test Organisation"
+
+echo "Creating test users for ${TEST_ORG_TITLE} Organisation:"
+
+add_user_if_needed ckan_user "CKAN User" ckan_user@localhost
+add_user_if_needed test_org_admin "Test Admin" test_org_admin@localhost
+add_user_if_needed test_org_editor "Test Editor" test_org_editor@localhost
+add_user_if_needed test_org_member "Test Member" test_org_member@localhost
+
+echo "Creating ${TEST_ORG_TITLE} Organisation:"
+
+TEST_ORG=$( \
+    curl -LsH "Authorization: ${API_KEY}" \
+    --data "name=${TEST_ORG_NAME}&title=${TEST_ORG_TITLE}" \
+    ${CKAN_ACTION_URL}/organization_create
+)
+
+TEST_ORG_ID=$(echo $TEST_ORG | sed -r 's/^(.*)"id": "(.*)",(.*)/\2/')
+
+echo "Assigning test users to ${TEST_ORG_TITLE} Organisation:"
+
+curl -LsH "Authorization: ${API_KEY}" \
+    --data "id=${TEST_ORG_ID}&object=test_org_admin&object_type=user&capacity=admin" \
+    ${CKAN_ACTION_URL}/member_create
+
+curl -LsH "Authorization: ${API_KEY}" \
+    --data "id=${TEST_ORG_ID}&object=test_org_editor&object_type=user&capacity=editor" \
+    ${CKAN_ACTION_URL}/member_create
+
+curl -LsH "Authorization: ${API_KEY}" \
+    --data "id=${TEST_ORG_ID}&object=test_org_member&object_type=user&capacity=member" \
+    ${CKAN_ACTION_URL}/member_create
+##
+# END.
+#
 
 # Creating test data hierarchy which creates organisations assigned to datasets
 ckan_cli create-test-data hierarchy
@@ -19,13 +67,118 @@ ckan_cli create-test-data hierarchy
 # Creating basic test data which has datasets with resources
 ckan_cli create-test-data
 
-echo "Updating annakarenina to use department-of-health Organisation:"
+# Datasets need to be assigned to an organisation
+
+echo "Assigning test Datasets to Organisation..."
+
+echo "Updating annakarenina to use ${TEST_ORG_TITLE} organisation:"
 package_owner_org_update=$( \
     curl -LsH "Authorization: ${API_KEY}" \
-    --data "id=annakarenina&organization_id=department-of-health" \
+    --data "id=annakarenina&organization_id=${TEST_ORG_NAME}" \
     ${CKAN_ACTION_URL}/package_owner_org_update
 )
 echo ${package_owner_org_update}
+
+echo "Updating warandpeace to use ${TEST_ORG_TITLE} organisation:"
+package_owner_org_update=$( \
+    curl -LsH "Authorization: ${API_KEY}" \
+    --data "id=warandpeace&organization_id=${TEST_ORG_NAME}" \
+    ${CKAN_ACTION_URL}/package_owner_org_update
+)
+echo ${package_owner_org_update}
+
+##
+# BEGIN: Create a Data Request organisation with test users for admin, editor and member and default data requests
+#
+# Data Requests requires a specific organisation to exist in order to create DRs for Data.Qld
+DR_ORG_NAME=open-data-administration-data-requests
+DR_ORG_TITLE="Open Data Administration (data requests)"
+
+echo "Creating test users for ${DR_ORG_TITLE} Organisation:"
+
+add_user_if_needed dr_admin "Data Request Admin" dr_admin@localhost
+add_user_if_needed dr_editor "Data Request Editor" dr_editor@localhost
+add_user_if_needed dr_member "Data Request Member" dr_member@localhost
+
+echo "Creating ${DR_ORG_TITLE} Organisation:"
+
+DR_ORG=$( \
+    curl -LsH "Authorization: ${API_KEY}" \
+    --data "name=${DR_ORG_NAME}&title=${DR_ORG_TITLE}" \
+    ${CKAN_ACTION_URL}/organization_create
+)
+
+DR_ORG_ID=$(echo $DR_ORG | sed -r 's/^(.*)"id": "(.*)",(.*)/\2/')
+
+echo "Assigning test users to ${DR_ORG_TITLE} Organisation:"
+
+curl -LsH "Authorization: ${API_KEY}" \
+    --data "id=${DR_ORG_ID}&object=dr_admin&object_type=user&capacity=admin" \
+    ${CKAN_ACTION_URL}/member_create
+
+curl -LsH "Authorization: ${API_KEY}" \
+    --data "id=${DR_ORG_ID}&object=dr_editor&object_type=user&capacity=editor" \
+    ${CKAN_ACTION_URL}/member_create
+
+curl -LsH "Authorization: ${API_KEY}" \
+    --data "id=${DR_ORG_ID}&object=dr_member&object_type=user&capacity=member" \
+    ${CKAN_ACTION_URL}/member_create
+
+
+echo "Creating test Data Request:"
+
+curl -LsH "Authorization: ${API_KEY}" \
+    --data "title=Test Request&description=This is an example&organization_id=${TEST_ORG_ID}" \
+    ${CKAN_ACTION_URL}/create_datarequest
+
+REPORT_ORG_NAME=reporting
+REPORT_ORG_TITLE=Reporting
+
+echo "Creating test users for ${REPORT_ORG_TITLE} Organisation:"
+
+add_user_if_needed report_admin "Reporting Admin" report_admin@localhost
+add_user_if_needed report_editor "Reporting Editor" report_editor@localhost
+
+echo "Creating ${REPORT_ORG_TITLE} Organisation:"
+
+REPORT_ORG=$( \
+    curl -LsH "Authorization: ${API_KEY}" \
+    --data "name=${REPORT_ORG_NAME}&title=${REPORT_ORG_TITLE}" \
+    ${CKAN_ACTION_URL}/organization_create
+)
+
+REPORT_ORG_ID=$(echo $REPORT_ORG | sed -r 's/^(.*)"id": "(.*)",(.*)/\2/')
+
+echo "Assigning test users to ${REPORT_ORG_TITLE} Organisation:"
+
+curl -LsH "Authorization: ${API_KEY}" \
+    --data "id=${REPORT_ORG_ID}&object=report_admin&object_type=user&capacity=admin" \
+    ${CKAN_ACTION_URL}/member_create
+
+curl -LsH "Authorization: ${API_KEY}" \
+    --data "id=${REPORT_ORG_ID}&object=report_editor&object_type=user&capacity=editor" \
+    ${CKAN_ACTION_URL}/member_create
+
+echo "Creating test dataset for reporting:"
+
+curl -LsH "Authorization: ${API_KEY}" \
+    --data "name=reporting&description=Dataset for reporting&owner_org=${REPORT_ORG_ID}&\
+update_frequency=near-realtime&author_email=report_admin@localhost&version=1.0&license_id=cc-by-4\
+&data_driven_application=NO&security_classification=PUBLIC&notes=test&de_identified_data=NO"\
+    ${CKAN_ACTION_URL}/package_create
+
+echo "Creating test Data Request for reporting:"
+
+curl -LsH "Authorization: ${API_KEY}" \
+    --data "title=Reporting Request&description=Data Request for reporting&organization_id=${REPORT_ORG_ID}" \
+    ${CKAN_ACTION_URL}/create_datarequest
+
+
+echo "Creating config value for resource formats:"
+
+curl -LsH "Authorization: ${API_KEY}" \
+    --data '{"ckanext.data_qld.resource_formats": "CSV\r\nHTML\r\nJSON\r\nRDF\r\nTXT\r\nXLS"}' \
+    ${CKAN_ACTION_URL}/config_option_update
 
 if [ "$VENV_DIR" != "" ]; then
   deactivate
--- test/features/steps/steps.py
+++ test/features/steps/steps.py
@@ -2,6 +2,10 @@ from behave import step
 from behaving.personas.steps import *  # noqa: F401, F403
 from behaving.web.steps import *  # noqa: F401, F403
 from behaving.web.steps.url import when_i_visit_url
+from behaving.mail.steps import *
+import email
+import quopri
+import random
 
 
 @step(u'I get the current URL')
@@ -37,18 +41,17 @@ def log_in_directly(context):
         When I fill in "login" with "$name"
         And I fill in "password" with "$password"
         And I press the element with xpath "//button[contains(string(), 'Login')]"
-        Then I should see an element with xpath "//a[contains(string(), 'Log out')]"
+        Then I should see an element with xpath "//a[@title='Log out']"
     """)
 
 
-@step('I go to dataset page')
-def go_to_dataset_page(context):
-    when_i_visit_url(context, '/dataset')
+@step('I fill in title with random text')
+def title_random_text(context):
 
-
-@step('I go to dataset "{name}"')
-def go_to_dataset(context, name):
-    when_i_visit_url(context, '/dataset/' + name)
+    assert context.persona
+    context.execute_steps(u"""
+        When I fill in "title" with "Test Title {0}"
+    """.format(random.randrange(1000)))
 
 
 @step('I go to organisation page')
@@ -59,3 +62,161 @@ def go_to_organisation_page(context):
 @step('I go to register page')
 def go_to_register_page(context):
     when_i_visit_url(context, '/user/register')
+
+
+@step('I log in and go to the data requests page')
+def log_in_go_to_datarequest_page(context):
+    assert context.persona
+    context.execute_steps(u"""
+        When I log in
+        And I go to the data requests page
+    """)
+
+
+@step('I go to the data requests page')
+def go_to_datarequest_page(context):
+    when_i_visit_url(context, '/datarequest')
+
+
+@step('I log in and create a datarequest')
+def log_in_create_a_datarequest(context):
+
+    assert context.persona
+    context.execute_steps(u"""
+        When I log in and go to the data requests page
+        And I create a datarequest
+    """)
+
+
+@step('I create a datarequest')
+def create_datarequest(context):
+
+    assert context.persona
+    context.execute_steps(u"""
+        When I go to the data requests page
+        And I click the link with text that contains "Add data request"
+        And I fill in title with random text
+        And I fill in "description" with "Test description"
+        And I press the element with xpath "//button[contains(string(), 'Create data request')]"
+    """)
+
+
+@step('I go to my reports page')
+def go_to_reporting_page(context):
+    when_i_visit_url(context, '/dashboard/reporting')
+
+
+@step('I go to dataset page')
+def go_to_dataset_page(context):
+    when_i_visit_url(context, '/dataset')
+
+
+@step(u'I go to dataset "{name}"')
+def go_to_dataset(context, name):
+    when_i_visit_url(context, '/dataset/' + name)
+
+
+@step(u'I go to dataset "{name}" comments')
+def go_to_dataset_comments(context, name):
+    context.execute_steps(u"""
+        When I go to dataset "%s"
+        And I click the link with text that contains "Comments"
+    """ % (name))
+
+
+@step(u'I go to data request "{subject}"')
+def go_to_data_request(context, subject):
+    context.execute_steps(u"""
+        When I go to the data requests page
+        And I click the link with text "%s"
+        Then I should see "%s" within 5 seconds
+    """ % (subject, subject))
+
+
+@step(u'I go to data request "{subject}" comments')
+def go_to_data_request_comments(context, subject):
+    context.execute_steps(u"""
+        When I go to data request "%s"
+        And I click the link with text that contains "Comments"
+    """ % (subject))
+
+
+@step(u'I set persona var "{key}" to "{value}"')
+def set_persona_var(context, key, value):
+    context.persona[key] = value
+
+
+@step(u'I submit a comment with subject "{subject}" and comment "{comment}"')
+def submit_comment_with_subject_and_comment(context, subject, comment):
+    """
+    There can be multiple comment forms per page (add, edit, reply) each with fields named "subject" and "comment"
+    This step overcomes a limitation of the fill() method which only fills a form field by name
+    :param context:
+    :param subject:
+    :param comment:
+    :return:
+    """
+    context.browser.execute_script(
+        "document.querySelector('form#comment_form input[name=\"subject\"]').value = '%s';" % subject)
+    context.browser.execute_script(
+        "document.querySelector('form#comment_form textarea[name=\"comment\"]').value = '%s';" % comment)
+    context.browser.execute_script(
+        "document.querySelector('form#comment_form .form-actions input[type=\"submit\"]').click();")
+
+
+@step(u'I submit a reply with comment "{comment}"')
+def submit_reply_with_comment(context, comment):
+    """
+    There can be multiple comment forms per page (add, edit, reply) each with fields named "subject" and "comment"
+    This step overcomes a limitation of the fill() method which only fills a form field by name
+    :param context:
+    :param comment:
+    :return:
+    """
+    context.browser.execute_script(
+        "document.querySelector('.comment-wrapper form textarea[name=\"comment\"]').value = '%s';" % comment)
+    context.browser.execute_script(
+        "document.querySelector('.comment-wrapper form .form-actions input[type=\"submit\"]').click();")
+
+
+@step('I create a dataset with license {license} and resource file {file}')
+def create_dataset_json(context, license, file):
+    create_dataset(context, license, 'JSON', file)
+
+
+@step('I create a dataset with license {license} and {file_format} resource file {file}')
+def create_dataset(context, license, file_format, file):
+    assert context.persona
+    context.execute_steps(u"""
+        When I visit "dataset/new"
+        And I fill in title with random text
+        And I fill in "notes" with "Description"
+        And I fill in "version" with "1.0"
+        And I fill in "author_email" with "test@me.com"
+        And I execute the script "document.getElementById('field-license_id').value={license}"
+        Then I select "NO" from "de_identified_data"
+        And I press "Add Data"
+        And I attach the file {file} to "upload"
+        And I fill in "name" with "Test Resource"
+        And I execute the script "document.getElementById('field-format').value={file_format}"
+        And I fill in "description" with "Test Resource Description"
+        And I press "Finish"
+    """.format(license=license, file=file, file_format=file_format))
+
+
+# The default behaving step does not convert base64 emails
+# Modifed the default step to decode the payload from base64
+@step(u'I should receive a base64 email at "{address}" containing "{text}"')
+def should_receive_base64_email_containing_text(context, address, text):
+    def filter_contents(mail):
+        mail = email.message_from_string(mail)
+        payload = mail.get_payload()
+        payload += "=" * ((4 - len(payload) % 4) % 4)  # do fix the padding error issue
+        payload_bytes = quopri.decodestring(payload)
+        if len(payload_bytes) > 0:
+            payload_bytes += b'='  # do fix the padding error issue
+        decoded_payload = payload_bytes.decode('base64')
+        print('decoded_payload: ', decoded_payload)
+        return text in decoded_payload
+
+    assert context.mail.user_messages(address, filter_contents)
