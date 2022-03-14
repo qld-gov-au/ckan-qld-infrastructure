67315406e7fe63ba7fe947dd030fc6c7d51c0502
ThrawnCA <shell_layer-github@yahoo.com.au>
2022-03-14 15:33:40 +1000

    [CHG-11299] drop cleanup scenario that may be causing a race condition

--- test/features/config.feature
+++ test/features/config.feature
@@ -1,10 +1,18 @@
 @config
-@OpenData
 Feature: Config
 
-    Scenario: Assert that configuration values are available
+    Scenario: Assert that configuration values are removed
         Given "SysAdmin" as the persona
-        When I log in
-        And I visit "ckan-admin/config"
+        When I log in and go to admin config page
+        Then I should see "Intro Text"
         And I should see "Suggested Description"
-        Then I should see an element with id "field-ckanext.data_qld.datarequest_suggested_description"
+        And I should see an element with id "field-ckanext.data_qld.excluded_display_name_words"
+        And I should not see an element with id "field-ckan-main-css"
+        And I should not see an element with id "field-ckan-site-custom-css"
+
+    @OpenData
+    Scenario: Assert that Data Request configuration values are available
+        Given "SysAdmin" as the persona
+        When I log in and go to admin config page
+        Then I should see "Suggested Description"
+        And I should see an element with id "field-ckanext.data_qld.datarequest_suggested_description"
--- test/features/steps/steps.py
+++ test/features/steps/steps.py
@@ -19,7 +19,10 @@ def go_to_home(context):
 
 @step(u'I go to register page')
 def go_to_register_page(context):
-    when_i_visit_url(context, '/user/register')
+    context.execute_steps(u"""
+        When I go to homepage
+        And I click the link with text that contains "Register"
+    """)
 
 
 @step(u'I log in')
@@ -56,7 +59,7 @@ def login_link_visible(context):
     """)
 
 
-@step('I fill in title with random text')
+@step(u'I fill in title with random text')
 def title_random_text(context):
 
     assert context.persona
@@ -101,7 +104,6 @@ def go_to_datarequest_page(context):
 
 @step(u'I log in and create a datarequest')
 def log_in_create_a_datarequest(context):
-
     assert context.persona
     context.execute_steps(u"""
         When I log in and go to the data requests page
@@ -190,12 +192,12 @@ def submit_reply_with_comment(context, comment):
         "document.querySelector('.comment-wrapper form .form-actions input[type=\"submit\"]').click();")
 
 
-@step('I create a dataset with license {license} and resource file {file}')
+@step(u'I create a dataset with license {license} and resource file {file}')
 def create_dataset_json(context, license, file):
     create_dataset(context, license, 'JSON', file)
 
 
-@step('I create a dataset with license {license} and {file_format} resource file {file}')
+@step(u'I create a dataset with license {license} and {file_format} resource file {file}')
 def create_dataset(context, license, file_format, file):
     assert context.persona
     context.execute_steps(u"""
@@ -216,7 +218,7 @@ def create_dataset(context, license, file_format, file):
 
 
 # The default behaving step does not convert base64 emails
-# Modifed the default step to decode the payload from base64
+# Modified the default step to decode the payload from base64
 @step(u'I should receive a base64 email at "{address}" containing "{text}"')
 def should_receive_base64_email_containing_text(context, address, text):
     def filter_contents(mail):
@@ -231,3 +233,17 @@ def should_receive_base64_email_containing_text(context, address, text):
         return text in decoded_payload
 
     assert context.mail.user_messages(address, filter_contents)
+
+
+@step(u'I log in and go to admin config page')
+def log_in_go_to_admin_config(context):
+    assert context.persona
+    context.execute_steps(u"""
+        When I log in
+        And I go to admin config page
+    """)
+
+
+@step(u'I go to admin config page')
+def go_to_admin_config(context):
+    when_i_visit_url(context, '/ckan-admin/config')
