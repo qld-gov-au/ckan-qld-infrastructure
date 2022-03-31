9e17c9df27ed303adddc9ff771255952c3aee896
ThrawnCA <shell_layer-github@yahoo.com.au>
2022-03-31 11:12:08 +1000

    [QOL-7905] move Open Data test data back to 'create-test-data' script as it needs the API key

--- .docker/scripts/init-OpenData.sh
+++ .docker/scripts/init-OpenData.sh
@@ -14,20 +14,3 @@ PASTER_PLUGIN=ckanext-report ckan_cli report initdb
 
 # Initialise the QA database tables
 PASTER_PLUGIN=ckanext-qa ckan_cli qa init
-
-##
-# BEGIN: Add sysadmin config values.
-# This needs to be done before closing datarequests as they require the below config values
-#
-echo "Adding ckan.datarequests.closing_circumstances:"
-
-curl -LsH "Authorization: ${API_KEY}" \
-    --data '{"ckan.datarequests.closing_circumstances":
-        "Released as open data|nominate_dataset\r\nOpen dataset already exists|nominate_dataset\r\nPartially released|nominate_dataset\r\nTo be released as open data at a later date|nominate_approximate_date\r\nData openly available elsewhere\r\nNot suitable for release as open data\r\nRequested data not available/cannot be compiled\r\nRequestor initiated closure"}' \
-    ${CKAN_ACTION_URL}/config_option_update
-
-echo "Creating config value for resource formats:"
-
-curl -LsH "Authorization: ${API_KEY}" \
-    --data '{"ckanext.data_qld.resource_formats": "CSV\r\nHTML\r\nJSON\r\nRDF\r\nTXT\r\nXLS"}' \
-    ${CKAN_ACTION_URL}/config_option_update
