600bb31ebfbf723292c3506e378249001629ffe9
ThrawnCA <shell_layer-github@yahoo.com.au>
2022-04-14 10:47:23 +1000

    [QOL-8594] retrieve default CKAN version if needed so Docker Compose doesn't pass a blank

--- retrieve-ckan-version.sh
+++ retrieve-ckan-version.sh
@@ -2,4 +2,12 @@
 
 TARGET_ENV=${DEPLOY_ENV:-DEV}
 EXTENSIONS_FILE=${EXTENSIONS_FILE:-vars/shared-$VARS_TYPE.var.yml}
-sed -e "/Environment: $TARGET_ENV/,/- Environment:/!d" $EXTENSIONS_FILE | grep CKANRevision |awk '{print $2}' |tr -d '"'
+CKAN_VERSION=$(sed -e "/Environment: $TARGET_ENV/,/- Environment:/!d" $EXTENSIONS_FILE | grep CKANRevision |awk '{print $2}' |tr -d '"')
+if [ "$CKAN_VERSION" = "" ]; then
+  CKAN_VERSION=$(grep 'ckan_qgov_branch' 'vars/CKAN-Stack.var.yml' | awk '{print $2}' |tr -d '"')
+fi
+if [ "$CKAN_VERSION" = "" ]; then
+  echo "No CKAN version found" >&2
+  exit 1
+fi
+echo $CKAN_VERSION
