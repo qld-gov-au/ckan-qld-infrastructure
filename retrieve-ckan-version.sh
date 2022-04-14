df7c920b7a05917fb5ff0d564101d793cdda6bb0
ThrawnCA <shell_layer-github@yahoo.com.au>
2022-04-14 11:21:32 +1000

    [QOL-8594] adjust script to use environment-specific CKAN version

--- retrieve-ckan-version.sh
+++ retrieve-ckan-version.sh
@@ -4,7 +4,12 @@ TARGET_ENV=${DEPLOY_ENV:-DEV}
 EXTENSIONS_FILE=${EXTENSIONS_FILE:-vars/shared-$VARS_TYPE.var.yml}
 CKAN_VERSION=$(sed -e "/Environment: $TARGET_ENV/,/- Environment:/!d" $EXTENSIONS_FILE | grep CKANRevision |awk '{print $2}' |tr -d '"')
 if [ "$CKAN_VERSION" = "" ]; then
-  CKAN_VERSION=$(grep 'ckan_qgov_branch:' 'vars/CKAN-Stack.var.yml' | awk '{print $2}' |tr -d '"')
+  CKAN_VERSION=$(sed -e "/Environment: $TARGET_ENV/,/template_parameters:/!d" 'vars/CKAN-Stack.var.yml' | grep CKANRevision | awk -F: '{print $2}' |tr -d '"')
+  if [ "$CKAN_VERSION" = "" ] || (echo "$CKAN_VERSION" | grep 'ckan_tag' >/dev/null); then
+    CKAN_VERSION=$(grep 'ckan_tag:' 'vars/CKAN-Stack.var.yml' | awk '{print $2}' |tr -d '"')
+  elif (echo "$CKAN_VERSION" | grep 'ckan_qgov_branch' >/dev/null); then
+    CKAN_VERSION=$(grep 'ckan_qgov_branch:' 'vars/CKAN-Stack.var.yml' | awk '{print $2}' |tr -d '"')
+  fi
 fi
 if [ "$CKAN_VERSION" = "" ]; then
   echo "No CKAN version found" >&2
