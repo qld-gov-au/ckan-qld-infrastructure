bc0ab9572622a0bec9032ba1011554d9b1fd95bc
ThrawnCA <shell_layer-github@yahoo.com.au>
2022-04-14 10:52:34 +1000

    [QOL-8594] fix regexp to identify test CKAN version
    - add a colon so we only retrieve the point where the value is defined, not everywhere it's used

--- retrieve-ckan-version.sh
+++ retrieve-ckan-version.sh
@@ -4,7 +4,7 @@ TARGET_ENV=${DEPLOY_ENV:-DEV}
 EXTENSIONS_FILE=${EXTENSIONS_FILE:-vars/shared-$VARS_TYPE.var.yml}
 CKAN_VERSION=$(sed -e "/Environment: $TARGET_ENV/,/- Environment:/!d" $EXTENSIONS_FILE | grep CKANRevision |awk '{print $2}' |tr -d '"')
 if [ "$CKAN_VERSION" = "" ]; then
-  CKAN_VERSION=$(grep 'ckan_qgov_branch' 'vars/CKAN-Stack.var.yml' | awk '{print $2}' |tr -d '"')
+  CKAN_VERSION=$(grep 'ckan_qgov_branch:' 'vars/CKAN-Stack.var.yml' | awk '{print $2}' |tr -d '"')
 fi
 if [ "$CKAN_VERSION" = "" ]; then
   echo "No CKAN version found" >&2
