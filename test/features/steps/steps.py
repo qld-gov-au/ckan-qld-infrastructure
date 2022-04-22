8c56a388e234d11aedd66303a6536fa21b5068fe
antuarc <carl.antuar@smartservice.qld.gov.au>
2022-04-22 10:56:48 +1000

    [QOL-8779] organise test steps by extension
    - this should make our test steps more readable and assist future cleanups

--- test/features/steps/steps.py
+++ test/features/steps/steps.py
@@ -5,7 +5,7 @@ from behaving.web.steps import *  # noqa: F401, F403
 from behaving.web.steps.url import when_i_visit_url
 import email
 import quopri
-import random
+import uuid
 
 
 @step(u'I get the current URL')
@@ -68,6 +68,24 @@ def login_link_visible(context):
     """)
 
 
+@step(u'I request a password reset')
+def request_reset(context):
+    assert context.persona
+    context.execute_steps(u"""
+        When I visit "/user/reset"
+        And I fill in "user" with "$name"
+        And I press the element with xpath "//button[contains(string(), 'Request Reset')]"
+        And I go to dataset page
+    """)
+
+
+@step(u'I fill in "{name}" with "{value"} if present')
+def fill_in_field_if_present(context, name, value):
+    context.execute_steps(u"""
+        When I execute the script "field = document.getElementById('field-{0}'); if (field) field.value = '{1}';"
+    """.format(name, value))
+
+
 @step(u'I create a resource with name "{name}" and URL "{url}"')
 def add_resource(context, name, url):
     context.execute_steps(u"""
@@ -76,7 +94,7 @@ def add_resource(context, name, url):
         And I execute the script "document.getElementById('field-image-url').value='{url}'"
         And I fill in "name" with "{name}"
         And I fill in "description" with "description"
-        And I execute the script "size_field = document.getElementById('field-size'); if (size_field) size_field.value = '1024';"
+        And I fill in "size" with "1024" if present
         And I press the element with xpath "//form[contains(@class, 'resource-form')]//button[contains(@class, 'btn-primary')]"
     """.format(name=name, url=url))
 
@@ -87,7 +105,7 @@ def title_random_text(context):
     assert context.persona
     context.execute_steps(u"""
         When I fill in "title" with "Test Title {0}"
-    """.format(random.randrange(100000)))
+    """.format(uuid.uuid4()))
 
 
 @step(u'I go to dataset page')
@@ -100,29 +118,6 @@ def go_to_dataset(context, name):
     when_i_visit_url(context, '/dataset/' + name)
 
 
-@step(u'I go to dataset "{name}" comments')
-def go_to_dataset_comments(context, name):
-    context.execute_steps(u"""
-        When I go to dataset "%s"
-        And I click the link with text that contains "Comments"
-    """ % (name))
-
-
-@step(u'I should see the add comment form')
-def comment_form_visible(context):
-    context.execute_steps(u"""
-        Then I should see an element with xpath "//textarea[@name='comment']"
-    """)
-
-
-@step(u'I should not see the add comment form')
-def comment_form_not_visible(context):
-    context.execute_steps(u"""
-        Then I should not see an element with xpath "//input[@name='subject']"
-        And I should not see an element with xpath "//textarea[@name='comment']"
-    """)
-
-
 @step(u'I edit the "{name}" dataset')
 def edit_dataset(context, name):
     when_i_visit_url(context, '/dataset/edit/{}'.format(name))
@@ -131,24 +126,6 @@ def edit_dataset(context, name):
 @step(u'I go to organisation page')
 def go_to_organisation_page(context):
     when_i_visit_url(context, '/organization')
-
-
-@step(u'I lock my account')
-def lock_account(context):
-    when_i_visit_url(context, "/user/login")
-    for x in range(11):
-        attempt_login(context, "incorrect password")
-
-
-@step(u'I request a password reset')
-def request_reset(context):
-    assert context.persona
-    context.execute_steps(u"""
-        When I visit "/user/reset"
-        And I fill in "user" with "$name"
-        And I press the element with xpath "//button[contains(string(), 'Request Reset')]"
-        And I go to dataset page
-    """)
 
 
 @step(u'I search the autocomplete API for user "{username}"')
@@ -186,97 +163,6 @@ def go_to_organisation_including_users(context, organisation_id, including):
     when_i_visit_url(context, r'/api/3/action/organization_show?id={}&include_users={}'.format(organisation_id, including in ['with', 'including']))
 
 
-@step(u'I log in and go to the data requests page')
-def log_in_go_to_datarequest_page(context):
-    assert context.persona
-    context.execute_steps(u"""
-        When I log in
-        And I go to the data requests page
-    """)
-
-
-@step(u'I go to the data requests page')
-def go_to_datarequest_page(context):
-    when_i_visit_url(context, '/datarequest')
-
-
-@step(u'I log in and create a datarequest')
-def log_in_create_a_datarequest(context):
-    assert context.persona
-    context.execute_steps(u"""
-        When I log in and go to the data requests page
-        And I create a datarequest
-    """)
-
-
-@step(u'I create a datarequest')
-def create_datarequest(context):
-
-    assert context.persona
-    context.execute_steps(u"""
-        When I go to the data requests page
-        And I click the link with text that contains "Add data request"
-        And I fill in title with random text
-        And I fill in "description" with "Test description"
-        And I press the element with xpath "//button[contains(@class, 'btn-primary')]"
-    """)
-
-
-@step(u'I go to my reports page')
-def go_to_reporting_page(context):
-    when_i_visit_url(context, '/dashboard/reporting')
-
-
-@step(u'I go to data request "{subject}"')
-def go_to_data_request(context, subject):
-    context.execute_steps(u"""
-        When I go to the data requests page
-        And I click the link with text "%s"
-        Then I should see "%s" within 5 seconds
-    """ % (subject, subject))
-
-
-@step(u'I go to data request "{subject}" comments')
-def go_to_data_request_comments(context, subject):
-    context.execute_steps(u"""
-        When I go to data request "%s"
-        And I click the link with text that contains "Comments"
-    """ % (subject))
-
-
-@step(u'I submit a comment with subject "{subject}" and comment "{comment}"')
-def submit_comment_with_subject_and_comment(context, subject, comment):
-    """
-    There can be multiple comment forms per page (add, edit, reply) each with fields named "subject" and "comment"
-    This step overcomes a limitation of the fill() method which only fills a form field by name
-    :param context:
-    :param subject:
-    :param comment:
-    :return:
-    """
-    context.browser.execute_script(
-        "document.querySelector('form#comment_form input[name=\"subject\"]').value = '%s';" % subject)
-    context.browser.execute_script(
-        "document.querySelector('form#comment_form textarea[name=\"comment\"]').value = '%s';" % comment)
-    context.browser.execute_script(
-        "document.querySelector('form#comment_form .form-actions input[type=\"submit\"]').click();")
-
-
-@step(u'I submit a reply with comment "{comment}"')
-def submit_reply_with_comment(context, comment):
-    """
-    There can be multiple comment forms per page (add, edit, reply) each with fields named "subject" and "comment"
-    This step overcomes a limitation of the fill() method which only fills a form field by name
-    :param context:
-    :param comment:
-    :return:
-    """
-    context.browser.execute_script(
-        "document.querySelector('.comment-wrapper form textarea[name=\"comment\"]').value = '%s';" % comment)
-    context.browser.execute_script(
-        "document.querySelector('.comment-wrapper form .form-actions input[type=\"submit\"]').click();")
-
-
 @step(u'I create a dataset with title "{title}"')
 def create_dataset_titled(context, title):
     context.execute_steps(u"""
@@ -285,7 +171,7 @@ def create_dataset_titled(context, title):
         And I fill in "notes" with "Description"
         And I fill in "version" with "1.0"
         And I fill in "author_email" with "test@me.com"
-        And I select "NO" from "de_identified_data"
+        And I fill in "de_identified_data" with "NO" if present
         And I press "Add Data"
         And I execute the script "document.getElementById('field-image-url').value='https://example.com'"
         And I fill in "name" with "Test Resource"
@@ -310,7 +196,7 @@ def create_dataset(context, license, file_format, file):
         And I fill in "version" with "1.0"
         And I fill in "author_email" with "test@me.com"
         And I execute the script "document.getElementById('field-license_id').value={license}"
-        Then I select "NO" from "de_identified_data"
+        Then I fill in "de_identified_data" with "NO" if present
         And I press "Add Data"
         And I attach the file {file} to "upload"
         And I fill in "name" with "Test Resource"
@@ -350,3 +236,136 @@ def log_in_go_to_admin_config(context):
 @step(u'I go to admin config page')
 def go_to_admin_config(context):
     when_i_visit_url(context, '/ckan-admin/config')
+
+
+# ckanext-ytp-comments
+
+
+@step(u'I go to dataset "{name}" comments')
+def go_to_dataset_comments(context, name):
+    context.execute_steps(u"""
+        When I go to dataset "%s"
+        And I click the link with text that contains "Comments"
+    """ % (name))
+
+
+@step(u'I should see the add comment form')
+def comment_form_visible(context):
+    context.execute_steps(u"""
+        Then I should see an element with xpath "//textarea[@name='comment']"
+    """)
+
+
+@step(u'I should not see the add comment form')
+def comment_form_not_visible(context):
+    context.execute_steps(u"""
+        Then I should not see an element with xpath "//input[@name='subject']"
+        And I should not see an element with xpath "//textarea[@name='comment']"
+    """)
+
+
+@step(u'I submit a comment with subject "{subject}" and comment "{comment}"')
+def submit_comment_with_subject_and_comment(context, subject, comment):
+    """
+    There can be multiple comment forms per page (add, edit, reply) each with fields named "subject" and "comment"
+    This step overcomes a limitation of the fill() method which only fills a form field by name
+    :param context:
+    :param subject:
+    :param comment:
+    :return:
+    """
+    context.browser.execute_script(
+        "document.querySelector('form#comment_form input[name=\"subject\"]').value = '%s';" % subject)
+    context.browser.execute_script(
+        "document.querySelector('form#comment_form textarea[name=\"comment\"]').value = '%s';" % comment)
+    context.browser.execute_script(
+        "document.querySelector('form#comment_form .form-actions input[type=\"submit\"]').click();")
+
+
+@step(u'I submit a reply with comment "{comment}"')
+def submit_reply_with_comment(context, comment):
+    """
+    There can be multiple comment forms per page (add, edit, reply) each with fields named "subject" and "comment"
+    This step overcomes a limitation of the fill() method which only fills a form field by name
+    :param context:
+    :param comment:
+    :return:
+    """
+    context.browser.execute_script(
+        "document.querySelector('.comment-wrapper form textarea[name=\"comment\"]').value = '%s';" % comment)
+    context.browser.execute_script(
+        "document.querySelector('.comment-wrapper form .form-actions input[type=\"submit\"]').click();")
+
+
+# ckanext-qgov
+
+
+@step(u'I lock my account')
+def lock_account(context):
+    when_i_visit_url(context, "/user/login")
+    for x in range(11):
+        attempt_login(context, "incorrect password")
+
+
+# ckanext-datarequests
+
+
+@step(u'I log in and go to the data requests page')
+def log_in_go_to_datarequest_page(context):
+    assert context.persona
+    context.execute_steps(u"""
+        When I log in
+        And I go to the data requests page
+    """)
+
+
+@step(u'I go to the data requests page')
+def go_to_datarequest_page(context):
+    when_i_visit_url(context, '/datarequest')
+
+
+@step(u'I go to data request "{subject}"')
+def go_to_data_request(context, subject):
+    context.execute_steps(u"""
+        When I go to the data requests page
+        And I click the link with text "%s"
+        Then I should see "%s" within 5 seconds
+    """ % (subject, subject))
+
+
+@step(u'I log in and create a datarequest')
+def log_in_create_a_datarequest(context):
+    assert context.persona
+    context.execute_steps(u"""
+        When I log in and go to the data requests page
+        And I create a datarequest
+    """)
+
+
+@step(u'I create a datarequest')
+def create_datarequest(context):
+
+    assert context.persona
+    context.execute_steps(u"""
+        When I go to the data requests page
+        And I click the link with text that contains "Add data request"
+        And I fill in title with random text
+        And I fill in "description" with "Test description"
+        And I press the element with xpath "//button[contains(@class, 'btn-primary')]"
+    """)
+
+
+@step(u'I go to data request "{subject}" comments')
+def go_to_data_request_comments(context, subject):
+    context.execute_steps(u"""
+        When I go to data request "%s"
+        And I click the link with text that contains "Comments"
+    """ % (subject))
+
+
+# ckanext-report
+
+
+@step(u'I go to my reports page')
+def go_to_reporting_page(context):
+    when_i_visit_url(context, '/dashboard/reporting')
