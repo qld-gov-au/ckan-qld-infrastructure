7f8826cb85ef72e45354e465f371e8ca18e33fd8
ThrawnCA <shell_layer-github@yahoo.com.au>
2022-06-10 13:47:45 +1000

    [QOL-8940] use JavaScript to populate 'format' field
    - On Publications it's not a simple 'select' tag, so this is needed

--- test/features/steps/steps.py
+++ test/features/steps/steps.py
@@ -181,7 +181,7 @@ def create_dataset_titled(context, title):
         And I press "Add Data"
         And I execute the script "document.getElementById('field-image-url').value='https://example.com'"
         And I fill in "name" with "Test Resource"
-        And I select "HTML" from "format"
+        And I execute the script "document.getElementById('field-format').value='HTML'"
         And I fill in "description" with "Test Resource Description"
         And I fill in "size" with "1024" if present
         And I press the element with xpath "//form[contains(@class, 'resource-form')]//button[contains(@class, 'btn-primary')]"
