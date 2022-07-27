e1c34ac79c89540b01824cb4b4867a36ea7919ff
ThrawnCA <shell_layer-github@yahoo.com.au>
2022-07-27 14:24:37 +1000

    [QOL-9055] run tests on both Py2 and Py3

--- .docker/scripts/init-ext.sh
+++ .docker/scripts/init-ext.sh
@@ -4,6 +4,26 @@
 #
 set -e
 
+install_requirements () {
+    PROJECT_DIR="$1"
+    for filename in requirements-$PYTHON_VERSION.txt requirements.txt pip-requirements.txt; do
+        if [ -f "$PROJECT_DIR/$filename" ]; then
+            pip install -r "$PROJECT_DIR/$filename"
+            return 0
+        fi
+    done
+}
+
+install_dev_requirements () {
+    PROJECT_DIR="$1"
+    for filename in dev-requirements-$PYTHON_VERSION.txt requirements-dev-$PYTHON_VERSION.txt requirements-dev.txt dev-requirements.txt; do
+        if [ -f "$PROJECT_DIR/$filename" ]; then
+            pip install -r "$PROJECT_DIR/$filename"
+            return 0
+        fi
+    done
+}
+
 if [ "$VENV_DIR" != "" ]; then
   . ${VENV_DIR}/bin/activate
 fi
@@ -11,8 +31,8 @@ pip install -r "requirements.txt"
 pip install -r "requirements-dev.txt"
 EXTENSIONS_FILE=$APP_DIR/scripts/extensions.yml python $(dirname $0)/generate-ext-requirements.py
 pip install --force-reinstall -r "/tmp/requirements-ext.txt"
-for req in `ls $VENV_DIR/src/ckanext-*/requirements.txt`; do
-    pip install -r "$req"
+for extension in . `ls -d $VENV_DIR/src/ckanext-*`; do
+    install_requirements $extension
 done
 
 if [ "$VENV_DIR" != "" ]; then
