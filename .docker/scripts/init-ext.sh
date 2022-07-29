6456b1c7308a234bcbcf6feb0dcb9ee8abdb10a8
ThrawnCA <shell_layer-github@yahoo.com.au>
2022-07-29 10:50:54 +1000

    [QOL-9055] install requirements both before and after extensions
    - Install before so we get priority when a version range is specified. Install again after to apply our pins

--- .docker/scripts/init-ext.sh
+++ .docker/scripts/init-ext.sh
@@ -28,6 +28,7 @@ install_requirements () {
 if [ "$VENV_DIR" != "" ]; then
   . ${VENV_DIR}/bin/activate
 fi
+install_requirements . dev-requirements requirements-dev
 EXTENSIONS_FILE=$APP_DIR/scripts/extensions.yml python $(dirname $0)/generate-ext-requirements.py
 pip install --force-reinstall -r "/tmp/requirements-ext.txt"
 for extension in . `ls -d $VENV_DIR/src/ckanext-*`; do
