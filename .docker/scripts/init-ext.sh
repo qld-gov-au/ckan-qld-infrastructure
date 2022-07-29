03339ec21bd0b591fe58b645f1e1c4d92276c764
ThrawnCA <shell_layer-github@yahoo.com.au>
2022-07-29 10:16:00 +1000

    [QOL-9055] update Archiver to fix Py3 compatibility
    - Drop obsolete Kombu dependency

--- .docker/scripts/init-ext.sh
+++ .docker/scripts/init-ext.sh
@@ -5,20 +5,21 @@
 set -e
 
 install_requirements () {
-    PROJECT_DIR="$1"
-    for filename in requirements-$PYTHON_VERSION.txt requirements.txt pip-requirements.txt; do
-        if [ -f "$PROJECT_DIR/$filename" ]; then
-            pip install -r "$PROJECT_DIR/$filename"
+    PROJECT_DIR=$1
+    shift
+    # Identify the best match requirements file, ignore the others.
+    # If there is one specific to our Python version, use that.
+    for filename_pattern in "$@"; do
+        filename="$PROJECT_DIR/${filename_pattern}-$PYTHON_VERSION.txt"
+        if [ -f "$filename" ]; then
+            pip install -r "$filename"
             return 0
         fi
     done
-}
-
-install_dev_requirements () {
-    PROJECT_DIR="$1"
-    for filename in dev-requirements-$PYTHON_VERSION.txt requirements-dev-$PYTHON_VERSION.txt requirements-dev.txt dev-requirements.txt; do
-        if [ -f "$PROJECT_DIR/$filename" ]; then
-            pip install -r "$PROJECT_DIR/$filename"
+    for filename_pattern in "$@"; do
+        filename="$PROJECT_DIR/$filename_pattern.txt"
+        if [ -f "$filename" ]; then
+            pip install -r "$filename"
             return 0
         fi
     done
@@ -27,12 +28,11 @@ install_dev_requirements () {
 if [ "$VENV_DIR" != "" ]; then
   . ${VENV_DIR}/bin/activate
 fi
-pip install -r "requirements.txt"
-pip install -r "requirements-dev.txt"
+install_requirements . dev-requirements requirements-dev
 EXTENSIONS_FILE=$APP_DIR/scripts/extensions.yml python $(dirname $0)/generate-ext-requirements.py
 pip install --force-reinstall -r "/tmp/requirements-ext.txt"
 for extension in . `ls -d $VENV_DIR/src/ckanext-*`; do
-    install_requirements $extension
+    install_requirements $extension requirements pip-requirements
 done
 
 if [ "$VENV_DIR" != "" ]; then
