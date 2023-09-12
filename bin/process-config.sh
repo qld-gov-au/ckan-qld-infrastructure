d72475cd49d5608b245a9e41fd6daf8a56322a84
ThrawnCA <shell_layer-github@yahoo.com.au>
2023-09-12 14:23:24 +1000

    [QOLSVC-978] adjust config processing to handle app-specific files

--- bin/process-config.sh
+++ bin/process-config.sh
@@ -1,5 +1,4 @@
 if [ -d "$SRC_DIR/ckan/ckanext/activity" ]; then
-    sed -i 's|^ckan.plugins =|ckan.plugins = activity|' $CKAN_INI
-    sed -i 's|^ckan.plugins =|ckan.plugins = activity|' .docker/test.ini
+    sed -i 's|^ckan.plugins =|ckan.plugins = activity|' $CKAN_INI .docker/test-*.ini
 fi
 
