80285be476a3909d250756d3f20f31ecd7cbca80
ThrawnCA <shell_layer-github@yahoo.com.au>
2024-03-28 14:21:59 +1000

    [QOLDEV-545] refactor CloudFormation template to restore SSM parameters
    - Split stack template to put IAM roles in a separate one, so that we have enough space in the size limit
    for both SSM Parameter Store entries and OpsWorks custom JSON

--- build-CKAN.sh
+++ build-CKAN.sh
@@ -53,6 +53,7 @@ run-all-playbooks () {
     run-playbook "database-config"
   fi
   run-playbook "CloudFormation" "vars/s3_buckets.var.yml"
+  run-playbook "instance-roles"
   run-playbook "CKAN-Stack"
   run-playbook "CloudFormation" "vars/instances-${INSTANCE_NAME}.var.yml"
   run-playbook "CloudFormation" "vars/cloudfront-lambda-at-edge.var.yml"
