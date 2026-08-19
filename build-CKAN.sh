ef58d131f35f95fbb3afed75b029c9c3c382c660
antuarc <carl.antuar@smartservice.qld.gov.au>
2026-08-19 11:59:05 +1000

    [QOLSVC-15918] Amazon Linux patching

--- build-CKAN.sh
+++ build-CKAN.sh
@@ -69,8 +69,8 @@ run-deployment () {
 
 create-baseline-ami () {
   # https://docs.aws.amazon.com/linux/al2023/release-notes/relnotes.html
-  # Amazon Linux 2023 AMI 2023.12.20260803.3 arm64 HVM kernel-6.12 (al2023-ami-2023.12.20260803.3-kernel-6.12-arm64) - 2026-08-03T17:37:49.000Z
-  VANILLA_IMAGE_ID="ami-0d2316cef187452bf"
+  # Amazon Linux 2023 AMI 2023.12.20260817.0 arm64 HVM kernel-6.12 (al2023-ami-2023.12.20260817.0-kernel-6.12-arm64) - 2026-08-12T23:49:31.000Z
+  VANILLA_IMAGE_ID="ami-0fe99bfb009b02407"
   read -r \
     LATEST_IMAGE_NAME \
     LATEST_VANILLA_IMAGE \
