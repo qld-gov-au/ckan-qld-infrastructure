8e5b010c47e2d19886d7dcb0f975c40ff846fb21
ThrawnCA <carl.antuar@smartservice.qld.gov.au>
2026-09-18 11:06:55 +1000

    operating system patching
    - Update to 2026-09-17 AMI

--- build-CKAN.sh
+++ build-CKAN.sh
@@ -69,8 +69,8 @@ run-deployment () {
 
 create-baseline-ami () {
   # https://docs.aws.amazon.com/linux/al2023/release-notes/relnotes.html
-  # Amazon Linux 2023 AMI 2023.12.20260914.0 arm64 HVM kernel-6.18 (al2023-ami-2023.12.20260914.0-kernel-6.18-arm64) - 2026-09-10T20:02:57.000Z
-  VANILLA_IMAGE_ID="ami-05d568276d4c3dac5"
+  # Amazon Linux 2023 AMI 2023.12.20260917.1 arm64 HVM kernel-6.1 (al2023-ami-2023.12.20260917.1-kernel-6.1-arm64) - 2026-09-17T00:56:48.000Z
+  VANILLA_IMAGE_ID="ami-0dbf004439107ab35"
   read -r \
     LATEST_IMAGE_NAME \
     LATEST_VANILLA_IMAGE \
